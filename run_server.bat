aa4aa4d16224552173976f30888b9289baf8b28b
BaoNghiaNghia <baonghia.kht@gmail.com>
2024-12-11 14:47:29 +0700

    update script

--- run_server.bat
+++ run_server.bat
@@ -1,27 +1,29 @@
-@echo off
-REM Stop and remove the container if it exists
-docker ps -a --filter "name=ytbthumbnailssc" --format "{{.Names}}" | findstr /I "ytbthumbnailssc" >nul
+@REM @echo off
+@REM REM Stop and remove the container if it exists
+@REM docker ps -a --filter "name=ytbthumbnailssc" --format "{{.Names}}" | findstr /I "ytbthumbnailssc" >nul
 
-IF %ERRORLEVEL% EQU 0 (
-    REM Container exists, stopping and removing the container
-    echo Container 'ytbthumbnailssc' exists. Stopping and removing the container...
-    docker stop ytbthumbnailssc
-    docker rm ytbthumbnailssc
-) ELSE (
-    REM Container does not exist
-    echo Container 'ytbthumbnailssc' does not exist.
-)
+@REM IF %ERRORLEVEL% EQU 0 (
+@REM     REM Container exists, stopping and removing the container
+@REM     echo Container 'ytbthumbnailssc' exists. Stopping and removing the container...
+@REM     docker stop ytbthumbnailssc
+@REM     docker rm ytbthumbnailssc
+@REM ) ELSE (
+@REM     REM Container does not exist
+@REM     echo Container 'ytbthumbnailssc' does not exist.
+@REM )
 
-REM Remove all previous Docker images
-echo Removing all previous Docker images...
-for /f "tokens=*" %%i in ('docker images -q') do docker rmi -f %%i
+@REM REM Remove all previous Docker images
+@REM echo Removing all previous Docker images...
+@REM for /f "tokens=*" %%i in ('docker images -q') do docker rmi -f %%i
 
-REM Rebuild the Docker image
-echo Rebuilding Docker image 'ytbthumbnailssc'...
-docker build -t ytbthumbnailssc .
+@REM REM Rebuild the Docker image
+@REM echo Rebuilding Docker image 'ytbthumbnailssc'...
+@REM docker build -t ytbthumbnailssc .
 
-REM Restart the container with the new image
-echo Starting the container with the new image...
-docker run -d -p 8000:8000 --env-file .env --name ytbthumbnailssc ytbthumbnailssc
+@REM REM Restart the container with the new image
+@REM echo Starting the container with the new image...
+@REM docker run -d -p 8000:8000 --env-file .env --name ytbthumbnailssc ytbthumbnailssc
 
-exit
+@REM exit
+
+uvicorn backend.main:app --reload --host 0.0.0.0 --port 8000
