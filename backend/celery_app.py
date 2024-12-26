f408e11129c1605e0b103411b86004959fe87ac2
BaoNghiaNghia <baonghia.kht@gmail.com>
2024-12-26 11:15:17 +0700

    update cron job

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -11,8 +11,8 @@ celery_app.conf.update(
     task_serializer="json",
     accept_content=["json"],
     result_serializer="json",
-    timezone="UTC",
-    enable_utc=True,
+    timezone="Asia/Ho_Chi_Minh",  # Set timezone to Vietnam
+    enable_utc=False,  # Disable UTC to use the local timezone
     broker_connection_retry_on_startup=True,  # Ensure retry on startup
 )
 
@@ -20,18 +20,15 @@ celery_app.conf.update(
 celery_app.conf.beat_schedule = {
     "check-and-generate-images": {
         "task": "backend.tasks.check_and_generate_images",
-        "schedule": crontab(minute="*/2", hour="20-23,0-5"),  # Every 2 minutes from 8 PM to 6 AM
-        # "schedule": crontab(minute="*/3"),  # Every 1 minutes
+        "schedule": crontab(minute="*/2", hour=[20,21,22,23,1,2,3,4,5,6], day_of_week="*"),
     },
     "delete-oldest-images-tool-render": {
         "task": "backend.tasks.delete_oldest_images_tool_render",
-        "schedule": crontab(hour=1, minute="*/5", day_of_week="*"),  # Every 5 minutes between 1:00 AM and 2:00 AM
-        # "schedule": crontab(minute="*/5"),  # Every 2 minutes
+        "schedule": crontab(hour=[1,2,3], minute="*/5", day_of_week="*"),
     },
     "delete-oldest-images-team-automation": {
         "task": "backend.tasks.delete_oldest_images_team_automation",
-        "schedule": crontab(hour=1, minute="*/5", day_of_week="*"),  # Every 5 minutes between 1:00 AM and 2:00 AM
-        # "schedule": crontab(minute="*/5"),  # Every 2 minutes
+        "schedule": crontab(hour=[1,2,3], minute="*/5", day_of_week="*"),
     },
 }
 
