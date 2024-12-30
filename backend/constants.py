8d474a2d2e64912fdb00cb8c4260917542fab677
BaoNghiaNghia <baonghia.kht@gmail.com>
2024-12-30 11:37:36 +0700

    update key

--- backend/constants.py
+++ backend/constants.py
@@ -65,41 +65,35 @@ THUMBNAIL_STYLE_LIST = [
 ]
 
 GEMINI_KEY_TOOL_RENDER = [
-    "AIzaSyDmIPvKb7iTGglaBICwzR5BD0tWhD4UL2k",
-    "AIzaSyBx2Q_qkWaxl_eQc1cjPQRkG5D-_sm2Yg4",
-    "AIzaSyCQhpPnmtnLTcUNRp98ZXp8m3khcyyjp_Y",
-    "AIzaSyBShFKq-s9T6UphIjrJhngBWjAdw9Osti8",
-    "AIzaSyDmUx2fh466J9FIYFI6tCRIFSrEZLGXLaY",
-    "AIzaSyB90zhWy91aD9QTIyHJa6jaY70apqnQfdw",
-    "AIzaSyCB6MkXy2KOLYMvIc0ln1D3PvAtLB9LIbQ"
+    "AIzaSyCRZYMR_cqC5XdJ6wi4cO7Tch-knO_WtW4",
+    "AIzaSyBDi2wGNLiAD6iqncKZrGRZThKg_z-3I6c",
+    "AIzaSyCk9FrUaN_C9245Z_B3DvtzDggWu2ksCBg",
+    "AIzaSyBIX2kBeAS21wJUU05sJDB_0su1PiLJz_w",
+    "AIzaSyDetmj90xmAkybYuuMM4DXUc4Tc0hIRj-M",
+    "AIzaSyDP91xrK7B6zjnf0jKeRudguSUO-GHyhmQ",
+    "AIzaSyB8xGKrZBVfosfMIYnddX68hypfwZSY6dA"
 ]
 
 GEMINI_KEY_TEAM_AUTOMATION = [
-    "AIzaSyA1Z5slGG7kbIOWinCnuz4OJiJ3a6G0t7Y",
-    "AIzaSyC1KVctwhDdVbIDv2cOZDa1kWyz0gq_jOQ",
+    "AIzaSyDlO7yQAKhOafl6N5FT8O6dtMI3DKEROpg",
+    "AIzaSyDsvs2FqI213De7KH0sPYhCjgVcxDsJhAs",
+    "AIzaSyDQWtvW0-J88JhQ2-pkhRjMc1EGn0S0q-A",
+    "AIzaSyDI87mVYJ1yqUdBWguUPOpMS1U441YRtdA",
+    "AIzaSyCbFla1wYfzexbnwyd25jayJSM8IOlXWuc",
+    "AIzaSyAR7gTd2tu_y1XsGeTnVf1hZiv6BHs_CZA",
+    "AIzaSyAGuUI4P2PcUj5n8AOnbtVQHf4H8hbv4ZU",
+    "AIzaSyBq1C036iuzXp3f3EHrUU84b1fCP16xdpY",
+    "AIzaSyD0cry8PM9mSltSrvQxXwxWyvO4iYBi_ZA",
+    "AIzaSyDxaXcenPFLOIu39R7DIsbEo2tvMUAd_Bc",
+    "AIzaSyA3yKhWEcKxJ5YDLjONUjUkvDLB3mBQmfc",
+    "AIzaSyBxrbjQUL2cu5QLdRBkdp6pW9cv31FrrJA",
+    "AIzaSyDxaXcenPFLOIu39R7DIsbEo2tvMUAd_Bc",
+    "AIzaSyBxrbjQUL2cu5QLdRBkdp6pW9cv31FrrJA",
+    "AIzaSyDRPU-XEQzzX13AMKo_At0St0uqEQWAlqA",
     "AIzaSyA85MP5jctMT9rKVR6gT16tEmsuO4JqpLg",
-    "AIzaSyCzXVTqFDI1a1XV5iLwIAcqY-bjR1Xpz8Y",
-    "AIzaSyChq25pUzIUTcIICrudPQi1rJiLHPjOCgA",
-    "AIzaSyDUFE187efrejWYzHuK_uhidSIQL9G3Vzk",
-    "AIzaSyBEpQWeTjyMAPdmWJ0Fqu61i6G54BbqbUo",
-    "AIzaSyAbyGg-WXEmnHV6hYIbzU3hpshyIBfJp5k",
-    "AIzaSyC7sF9TPticbbOJbo82yuuqGJyjIvTgFsk",
-    "AIzaSyA4KM3KP2s1Wtf7WSK0Cf-UVK8DBHrDl84",
-    "AIzaSyCjV7cuw7lm6RHenZl2SMPEOXXDSZhI5T8",
-    "AIzaSyAO5XPik3WO225cp5vQXagi-BOmhjNkTzc",
-    "AIzaSyCywQKy6D5E-yAzidTldWVhuPttZKKwM_Q",
-    "AIzaSyDwTSVpdJ4nO5Ql9bnkpdEv4xMtnL_Hays",
-    "AIzaSyBs_cp1hrXDYHmxHQufvqi9WhiLS8Iw_GE",
-    "AIzaSyBNA_3L1JXOSn5s8NnfFRPXUTYYiEbreW0",
-    "AIzaSyDBoL5qnSzGvNtjl0oOa3tWKvyJUcYypO0",
-    "AIzaSyCrB_c2gN6zHRCfR5BtsebqpWEVlm4Byvc",
-    "AIzaSyDmIPvKb7iTGglaBICwzR5BD0tWhD4UL2k",
-    "AIzaSyBx2Q_qkWaxl_eQc1cjPQRkG5D-_sm2Yg4",
-    "AIzaSyCQhpPnmtnLTcUNRp98ZXp8m3khcyyjp_Y",
-    "AIzaSyBShFKq-s9T6UphIjrJhngBWjAdw9Osti8",
-    "AIzaSyDmUx2fh466J9FIYFI6tCRIFSrEZLGXLaY",
-    "AIzaSyB90zhWy91aD9QTIyHJa6jaY70apqnQfdw",
-    "AIzaSyCB6MkXy2KOLYMvIc0ln1D3PvAtLB9LIbQ"
+    "AIzaSyCDzXlBfi5lRt2iLY5PT3NSodB-uVbcDuY",
+    "AIzaSyDJ2tjhP0xxDv7EJkJZwe0_g50qULOTDno",
+    "AIzaSyAV0LDFJk_ANuVW6ZHteJAUzF5U0Mq9BBE",
 ]
 
 SUBFOLDER_TOOL_RENDER = 'tool_render'
