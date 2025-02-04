d7a1fa7f78de99c7dd50b7b631cc07fa6c4206d9
BaoNghiaNghia <baonghia.kht@gmail.com>
2025-02-04 11:27:00 +0700

    update constant

--- backend/celery_app.py
+++ backend/celery_app.py
@@ -20,7 +20,7 @@ celery_app.conf.update(
 celery_app.conf.beat_schedule = {
     "check-and-generate-images": {
         "task": "backend.tasks.check_and_generate_images",
-        "schedule": crontab(minute="*/1", hour=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23], day_of_week="*"),
+        "schedule": crontab(minute="*/1", hour=[0,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23], day_of_week="*"),
     },
     "delete-oldest-images-tool-render": {
         "task": "backend.tasks.delete_oldest_images_tool_render",
